656ca23a8766b8180f1acf811580de97fa1c99f0
hy.moon <moraramee@gmail.com>
2022-06-23 21:35:31 +0900

    config: Add security filter chain

--- member/backend/src/main/java/com/gaj/member/config/SecurityConfig.java
+++ member/backend/src/main/java/com/gaj/member/config/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.gaj.member.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().permitAll()
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable();
+        return http.build();
+    }
 }
