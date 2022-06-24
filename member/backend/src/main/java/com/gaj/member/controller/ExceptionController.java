a8b6848b304898c27c9ae108ae1b95d7fb6a4e2b
hy.moon <moraramee@gmail.com>
2022-06-24 21:09:36 +0900

    feat: Add exceptions
    Add no handler exception
    Add exception handler for remain errors

--- member/backend/src/main/java/com/gaj/member/controller/ExceptionController.java
+++ member/backend/src/main/java/com/gaj/member/controller/ExceptionController.java
@@ -6,24 +6,37 @@ import com.gaj.member.exception.MemberException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionController {
-    private String VALIDATION_ERROR = "유효성 검증에 실패했습니다.";
+
+    @ExceptionHandler({
+            HttpRequestMethodNotSupportedException.class
+    })
+    public ResponseEntity<RestResponse> methodNotAllowedError() {
+        RestResponse restResponse = RestResponse.builder()
+                .status(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .message(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase())
+                .build();
+        return new ResponseEntity<RestResponse>(restResponse, HttpStatus.METHOD_NOT_ALLOWED);
+    }
 
     @ExceptionHandler({
             MethodArgumentNotValidException.class,
             BindException.class
     })
     public ResponseEntity<RestResponse> validationError(BindException exception) {
+
         RestResponse restResponse = RestResponse.builder()
-                .code(HttpStatus.BAD_REQUEST.value())
-                .status(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .status(HttpStatus.BAD_REQUEST.value())
                 .message(exception.getBindingResult().getAllErrors().stream().map(o -> o.getDefaultMessage()).collect(Collectors.joining()))
                 .build();
         return new ResponseEntity<RestResponse>(restResponse, HttpStatus.BAD_REQUEST);
@@ -33,11 +46,35 @@ public class ExceptionController {
             MemberException.class
     })
     public ResponseEntity<RestResponse> memberNotExistError(MemberException exception) {
+
         RestResponse restResponse = RestResponse.builder()
-                .code(HttpStatus.BAD_REQUEST.value())
-                .status(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .status(HttpStatus.BAD_REQUEST.value())
                 .message(exception.getMessage())
                 .build();
         return new ResponseEntity<RestResponse>(restResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({
+            NoHandlerFoundException.class
+    })
+    public ResponseEntity<RestResponse> noHandlerFoundError(NoHandlerFoundException exception, WebRequest request) {
+
+        RestResponse restResponse = RestResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .build();
+        return new ResponseEntity<RestResponse>(restResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({
+            Exception.class
+    })
+    public ResponseEntity<RestResponse> remainError(Exception exception) {
+
+        RestResponse restResponse = RestResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .build();
+        return new ResponseEntity<RestResponse>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
