cfb0dade9239e9ab9db37bc1f32f0aed0372861d
hy.moon <moraramee@gmail.com>
2022-06-24 21:33:27 +0900

    feat: Add social random password

--- member/backend/src/main/java/com/gaj/member/service/MemberService.java
+++ member/backend/src/main/java/com/gaj/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.gaj.member.dto.social.UserInfoResponseDto;
 import com.gaj.member.exception.MemberException;
 import com.gaj.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,24 +21,27 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final GoogleService googleService;
 
+    private int TEMP_PASSWORD_SIZE = 12;
+
     public String login(Member member) throws MemberException {
+
         Optional<Member> targetMember = memberRepository.findByEmail(member.getEmail());
 
-        if (targetMember.isPresent() &&
-                targetMember.get().validPassword(passwordEncoder, member.getPassword())) {
-            return "{JWT}";
+        if (targetMember.isPresent() && targetMember.get().validPassword(passwordEncoder, member.getPassword())) {
+            return "{LOGGED_IN_JWT}";
         } else {
             throw MemberException.MEMBER_NOT_EXISTS_EXCEPTION;
         }
     }
 
     public String signup(Member member) {
+
         Optional<Member> targetMember = memberRepository.findByEmail(member.getEmail());
 
         if (targetMember.isEmpty()) {
             member.setPassword(passwordEncoder.encode(member.getPassword()));
             memberRepository.save(member);
-            return "{JWT}";
+            return "{SIGNED_UP_JWT}";
         } else {
             throw MemberException.MEMBER_ALREADY_EXISTS_EXCEPTION;
         }
@@ -46,15 +50,23 @@ public class MemberService {
 
     public String socialLogin(Member member) {
 
-        // return JWT
-        return "{JWT}";
+        Optional<Member> targetMember = memberRepository.findByEmail(member.getEmail());
+
+        if (targetMember.isPresent()) {
+
+            return "{SOCIAL_LOGGED_IN_JWT}";
+        } else {
+
+            throw MemberException.MEMBER_NOT_EXISTS_EXCEPTION;
+        }
     }
 
     public String socialSignup(Member member) {
 
+        String tempPassword = RandomString.make(TEMP_PASSWORD_SIZE);
+        member.setPassword(passwordEncoder.encode(tempPassword));
         memberRepository.save(member);
-        // return JWT
-        return "{JWT}";
+        return "{SOCIAL_SIGNED_UP_JWT}";
     }
 
     // JWT return
